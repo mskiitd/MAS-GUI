c14bc930452e6225bb26ec597d68628ac2517390
nikhilbchilwant <nikhilbchilwant@gmail.com>
2015-04-06 23:09:29 +0530

    append to prev commit

---
+++ AgentProxy/src/mas/customerproxy/gui/DisplayOperationFrame.java
@@ -0,0 +1,39 @@
+package mas.customerproxy.gui;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import mas.jobproxy.job;
+import net.miginfocom.swing.MigLayout;
+
+@SuppressWarnings("serial")
+public class DisplayOperationFrame extends JFrame{
+
+	
+	public DisplayOperationFrame(job passedJob, int numOps, job populatingJob) {
+		setLayout(new MigLayout("","10","10"));
+		
+		for(int i=0;i<numOps;i++){
+			JLabel opTitle=new JLabel("operation #"+i+" : ");
+			add(opTitle);
+			
+			JLabel opNamelbl =new JLabel(passedJob.getOperations().get(i).
+					getJobOperationType());
+			add(opNamelbl,"wrap");
+		}
+		
+		setTitle("Operation details");
+//		setPreferredSize(new Dimension(600,500));
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		pack();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int centerX = (int)screenSize.getWidth() / 2;
+		int centerY = (int)screenSize.getHeight() / 2;
+		setLocation(centerX - getWidth() / 2, centerY - getHeight() / 2);
+		super.setVisible(true);
+	}
+}
---
+++ AgentProxy/src/mas/customerproxy/plan/ChangeDueDate.java
@@ -0,0 +1,75 @@
+package mas.customerproxy.plan;
+
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.UnreadableException;
+
+import java.util.ArrayList;
+
+import mas.customerproxy.agent.CustomerAgent;
+import mas.customerproxy.gui.ChangeDueDateGUI;
+import mas.jobproxy.Batch;
+import mas.jobproxy.job;
+import mas.jobproxy.jobOperation;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import bdi4jade.message.MessageGoal;
+import bdi4jade.plan.PlanBody;
+import bdi4jade.plan.PlanInstance;
+import bdi4jade.plan.PlanInstance.EndState;
+
+public class ChangeDueDate extends OneShotBehaviour implements PlanBody {
+
+	private Logger log=LogManager.getLogger();
+	private Batch batchToChangeDueDate;
+
+	@Override
+	public void init(PlanInstance PI) {
+		try {
+			batchToChangeDueDate=(Batch)((MessageGoal)(PI.getGoal())).
+					getMessage().getContentObject();
+		} catch (UnreadableException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void action() {
+		
+		Batch batchTosend=batchToChangeDueDate;
+		batchTosend.setBatchNumber(-1);
+		
+		/*batchTosend.setCost(batchToChangeDueDate.getCost());
+		batchTosend.setCPN(batchToChangeDueDate.getCPN());
+		batchTosend.setGenerationTime(System.currentTimeMillis());
+		batchTosend.setPenaltyRate(batchToChangeDueDate.getPenaltyRate());
+		batchTosend.setProfit(batchToChangeDueDate.getProfit());*/
+		job tempJob=batchToChangeDueDate.getSampleJob();
+		ArrayList<jobOperation> ops=tempJob.getOperations();
+		for(int n=0;n<batchToChangeDueDate.getCurrentOperationNumber();n++){
+			ops.remove(n);
+		}
+		
+		tempJob.setOperations(ops);
+		tempJob.resetOpnNoToZero();
+
+		ArrayList<job> jobArray=new ArrayList<job>();
+		for(int count=0;count<batchTosend.getBatchCount();count++){
+			jobArray.add(tempJob);
+		}
+		batchTosend.setJobsInBatch(jobArray);
+		
+		ChangeDueDateGUI gui = new ChangeDueDateGUI((CustomerAgent)myAgent, batchTosend);
+		
+	}
+
+	@Override
+	public EndState getEndState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
